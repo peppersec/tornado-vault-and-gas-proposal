94505a32beaf257e0ec8b6c86c1e0f09ef5ccf4d
Tisamenus <zeroexzerozerodead@protonmail.com>
2021-08-16 19:45:30 +0200

    adding helper functions for lottery

--- contracts/TornadoLotteryFunctionality.sol
+++ contracts/TornadoLotteryFunctionality.sol
@@ -6,10 +6,14 @@ import {Governance} from "../tornado-governance/contracts/Governance.sol";
 import {LotteryRandomNumberConsumer} from "./LotteryRandomNumberConsumer.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ABDKMath64x64} from "abdk-libraries-solidity/ABDKMath64x64.sol";
+import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
 
-// will be inherited by either a governance upgrade or a seperate contract 
+// will be inherited by either a governance upgrade or a seperate contract
 
 contract TornadoLotteryFunctionality is LotteryRandomNumberConsumer, Ownable {
+    using SafeMath for uint256;
+    using SafeMath for uint128;
+
     enum LotteryState {
         NotInitialized,
         RegisteringVoters,
@@ -18,28 +22,82 @@ contract TornadoLotteryFunctionality is LotteryRandomNumberConsumer, Ownable {
         PreparingNewRound
     }
 
+    enum ProposalStateAndValidity {
+        InvalidProposalForLottery,
+        ValidProposalForLottery,
+        ExecutedProposal
+    }
+
     struct UserVotingData {
-	    uint256 lotteryPower;
+        uint256 tornSquareRoot;
     }
 
     Governance public TornadoGovernance;
-    mapping(address => mapping(uint256 => UserVotingData)) public idToUserVotingData;
+    mapping(address => mapping(uint256 => UserVotingData))
+        public idToUserVotingData;
     address[][] public votersForProposal;
+    mapping(uint256 => ProposalStateAndValidity) public proposalWhitelist;
 
     constructor(address _governance) public LotteryRandomNumberConsumer() {
         TornadoGovernance = Governance(_governance);
     }
 
+    function _calculateSquareRoot(uint256 number) internal returns (uint256) {
+        uint128 number64 = uint128((number << 64).div(1e18));
+        uint128 squareRoot64 = ABDKMath64x64.sqrt(number64);
+        return uint256(((squareRoot64).mul(1e18)) >> 64);
+    }
+
+    function _setTornSquareRootOfAccount(address account, uint256 proposalId)
+        internal
+    {
+        idToUserVotingData[account][proposalId]
+            .tornSquareRoot = _calculateSquareRoot(
+            Governance.proposals[proposalId].receipts[account].votes
+        );
+    }
+
     function _registerAccountWithLottery(uint256 proposalId) internal {
-	    require(_checkIfAccountHasVoted(msg.sender, proposalId), "Account has not voted on this proposal");
-	    votersForProposal[proposalId][votersForProposal.length] = msg.sender;
+        require(
+            _checkIfProposalHasEnded(proposalId),
+            "Proposal has not finished yet"
+        );
+        require(
+            _checkIfAccountHasVoted(msg.sender, proposalId),
+            "Account has not voted on this proposal"
+        );
+	require(
+	    _checkIfProposalIsValid(proposalId),
+	    "Proposal not whitelisted"
+	);
+        votersForProposal[proposalId].push(msg.sender);
+        _setTornSquareRootOfAccount(msg.sender, proposalId);
     }
 
-    function _checkIfAccountHasVoted(address account, uint256 proposalId) internal view returns (bool) {
-	    return Governance.proposals[proposalId].receipts[account].hasVoted;
+    function _checkIfProposalHasEnded(uint256 proposalId)
+        internal
+	view
+        returns (bool)
+    {
+        return ((Governance.state(proposalId) ==
+            Governance.ProposalState.Executed) ||
+            (Governance.state(proposalId) ==
+                Governance.ProposalState.Defeated));
     }
 
-    function _calculateSquareRoot(uint256 number) internal returns (uint256);
+    function _checkIfAccountHasVoted(address account, uint256 proposalId)
+        internal
+        view
+        returns (bool)
+    {
+        return Governance.proposals[proposalId].receipts[account].hasVoted;
+    }
 
-    function _calculateLotteryPower(address account, uint256 proposalId) internal;
+    function _checkIfProposalIsValid(uint256 proposalId)
+        internal
+	view
+	returns (bool)
+    {
+	return (proposalWhitelist[proposalId] == ProposalStateAndValidity.ValidProposalForLottery);
+    }
 }
