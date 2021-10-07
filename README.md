1546e41de57e64faa20d2617b86baa185566dc0e
Tisamenus <zeroexzerozerodead@protonmail.com>
2021-10-07 20:45:20 +0200

    update readme + wait 6 blocks instead of 5 to be sure

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 Repository for a proposal to upgrade governance logic with automatic gas compensation functionality and extending the voting period for proposals.
 
-## Setup
+## Deployment
 
 ```bash
 yarn
 cp .env.example .env # fill it out
-yarn test
+yarn deploy <number of seconds for voting period>
 ```
--- tasks/deploy_proposal.js
+++ tasks/deploy_proposal.js
@@ -10,19 +10,19 @@ task('deploy_proposal', 'deploy the lottery/vault upgrade proposal')
     )
     const GasVaultContract = await GasVaultFactory.deploy()
 
-    await GasVaultContract.deployTransaction.wait(5)
+    await GasVaultContract.deployTransaction.wait(6)
 
     await hre.run('verify:verify', {
       address: GasVaultContract.address,
     })
 
-    const ProposalFactory = await hre.ethers.getContractFactory('LotteryAndVaultProposal')
+    const ProposalFactory = await hre.ethers.getContractFactory('VaultAndGasProposal')
     const ProposalContract = await ProposalFactory.deploy(
       GasVaultContract.address,
       BigNumber.from(taskArgs.votingPeriod),
     )
 
-    await ProposalContract.deployTransaction.wait(5)
+    await ProposalContract.deployTransaction.wait(6)
 
     await hre.run('verify:verify', {
       address: ProposalContract.address,
